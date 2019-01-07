1ee41d40cca50c9efefdb25f74dc9eb2da52c721
Jean-Michel <Jean-Michel.Brankart@univ-grenoble-alpes.fr>
2019-01-07 18:43:30 +0100

    Initial python signatures and wrappers

---
+++ python/signatures/propose_pyf.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+
+SRCDIR="$HOME/src/EnsDAM/src"
+
+for ffile in `ls ${SRCDIR}/*/*.[fF]90` ; do
+ modname=$(basename $(basename $ffile .f90) .F90)
+ if [ $# -eq 1 ];  then
+   if [ $1 = $modname ]; then
+     echo "Proposing signature file for: $modname"
+     f2py --overwrite-signature -m ${modname} -h ${modname}.pyf-proposed $ffile
+   fi
+ else
+   echo "Proposing signature file for: $modname"
+   f2py --overwrite-signature -m ${modname} -h ${modname}.pyf-proposed $ffile
+ fi
+done
