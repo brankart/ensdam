85fedd472f1a8b1db51725137daa3d8affcf6c6c
jmbrankart <Jean-Michel.Brankart@univ-grenoble-alpes.fr>
2026-05-05 16:15:06 +0200

    add compilation option for adastra genoa

--- tools/install_mpi.bash
+++ tools/install_mpi.bash
@@ -1,12 +1,35 @@
 #!/bin/bash
 #
 
-module purge
-module load intel-all/19.0.4
-module load hdf5/1.8.21-mpi
-module load netcdf/4.7.2-mpi
-module load netcdf-fortran/4.5.2-mpi
-module load netcdf-cxx4/4.3.1-mpi
+cluster='jean-zay'
+cluster='adastra'
+
+if [ $cluster = 'jean-zay' ] ; then
+
+  module purge
+  module load intel-all/19.0.4
+  module load hdf5/1.8.21-mpi
+  module load netcdf/4.7.2-mpi
+  module load netcdf-fortran/4.5.2-mpi
+  module load netcdf-cxx4/4.3.1-mpi
+
+elif [ $cluster = 'adastra' ] ; then
+
+  module purge
+  module load cpe/24.07
+  module load craype-x86-genoa
+  module load PrgEnv-cray
+  module load cray-mpich
+  module load cray-hdf5-parallel
+  module load cray-netcdf-hdf5parallel
+
+else
+
+  echo "Bad cluster name"
+  exit
+
+fi
+
 
 ln -sf macro/make.jean-zay_ifort Makefile.macro
 
